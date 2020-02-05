3447ad27e0a00c451127370b6c2c35aff4113894
Jesus Vega <jesusvega00@gmail.com>
2020-02-05 10:40:59 -0800

    adds intial files

--- server/app.js
+++ server/app.js
@@ -1,7 +1,34 @@
 const express = require('express');
-
-
+const morgan = require('morgan');
+const axios = require('axios');
 const app = express();
+const cache = {};
+
+app.use(morgan('dev'));
+app.get('/', function(req, res){
+        var movieId = req.query;
+        var key = Object.keys(movieId);
+        var value = Object.values(movieId);
+        var url = 'http://www.omdbapi.com/?apikey=8730e0e&'  + key + '=' + encodeURI(value);
+        
+        if (cache.hasOwnProperty(value)){
+            res.json(cache[value]);
+        } else {
+            console.log(cache);
+            axios.get(url)
+            .then(response => { 
+            cache[value] =  response.data;
+            res.send(response.data);
+            }).catch(err => res.json(err.message));
+        }
+});
+
+//http://www.omdbapi.com/?i=tt3896198&apikey=
+ 
+// var url = 'http://www.omdbapi.com/?apikey=1c41c0f5&'  
+
+
+
 
 // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
 
